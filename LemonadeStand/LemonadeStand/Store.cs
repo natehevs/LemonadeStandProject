2b787bdd0fe7d0e4a27aa1d7dfb8e8a083b3b99a
patrick hevrdejs <nathanhevrdejs@gmail.com>
2019-10-30 15:34:16 -0500

    added a switch case to Store class for the items in the store

--- LemonadeStand/LemonadeStand/Store.cs
+++ LemonadeStand/LemonadeStand/Store.cs
@@ -21,6 +21,46 @@ namespace LemonadeStand
 
         public void DisplayStoreOptions()
         {
+            Console.WriteLine("Welcome to the store, you can buy all your necessary ingredients for a lemonade stand here. Type 'lemon', 'cup', 'sugar', or 'ice' to view options and pricing.");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "lemon":
+                    BuyLemons();
+                    break;
+                case "cup":
+                    BuyCup();
+                    break;
+                case "sugar":
+                    BuySugar();
+                    break;
+                case "ice":
+                    BuyIce();
+                    break;
+                default:
+                    Console.WriteLine("Your input was not valid.");
+                    DisplayStoreOptions();
+                    break;
+            }
+        }
+
+        public void BuyLemons()
+        {
+            
+        }
+
+        public void BuyCup()
+        {
+
+        }
+
+        public void BuyIce()
+        {
+
+        }
+
+        public void BuySugar()
+        {
 
         }
 
